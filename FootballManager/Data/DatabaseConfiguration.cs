8b8fea9fa58a6b6ad67326e6049e56436c2a9549
KristiyanBandrov <97550267+KristiyanBandrov@users.noreply.github.com>
2022-03-21 22:05:36 +0200

    Change of database connection string

--- FootballManager/Data/DatabaseConfiguration.cs
+++ FootballManager/Data/DatabaseConfiguration.cs
@@ -4,6 +4,6 @@
     {
         // ReSharper disable once InconsistentNaming
         public const string ConnectionString =
-            @"Server=DESKTOP-A0032I6\SQLEXPRESS;Database=FootballManager;Trusted_Connection=True;Integrated Security=True;";
+            @"***";
     }
 }
